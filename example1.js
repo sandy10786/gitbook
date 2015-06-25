ed32364c1535048ba28ee4e60994c407a360ed4c
sandy10786 <sandy10786@gmail.com>
2015-06-25 17:05:48 +0530

    Example1

---
+++ example1.js
@@ -0,0 +1,3 @@
+var c=require('/home/sandeepchouhan/myapp/myapp/gitbook/myExport.js');
+var t=c(3);
+console.log(t); 
---
+++ myExport.js
@@ -0,0 +1,12 @@
+function Circle(r)
+{
+
+	function area()
+	{
+		return parseInt(r)*parseInt(r);
+	}
+	return area();
+}
+
+
+module.exports =  Circle;
